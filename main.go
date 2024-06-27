fbe0b0729b505e06c51d8fc3b24ced7f41b24d88
nharu-0630 <8305330+nharu-0630@users.noreply.github.com>
2024-06-27 19:17:51 +0900

    [add] ユーザ群を追加

--- main.go
+++ main.go
@@ -1,32 +1,31 @@
 package main
 
 import (
-	"flag"
-
-	"github.com/nharu-0630/twitter-api-client/cmd"
+	"github.com/nharu-0630/twitter-api-client/example"
 	"github.com/nharu-0630/twitter-api-client/tools"
-	"go.uber.org/zap"
 )
 
 func main() {
 	tools.LoadEnv()
 	tools.SetZapGlobals()
-	cmdType := flag.String("cmd", "", "クライアントコマンドの種類 (必須)")
-	flag.Parse()
-	if *cmdType == "" {
-		zap.L().Fatal("Cmd type is required")
-	}
-	switch *cmdType {
-	case "UserFollowers":
-		cmd := cmd.UserFollowersCmdFromFlag()
-		cmd.Execute()
-	case "UserID":
-		cmd := cmd.UserIDCmdFromFlag()
-		cmd.Execute()
-	case "UserFollowings":
-		cmd := cmd.UserFollowingsCmdFromFlag()
-		cmd.Execute()
-	default:
-		zap.L().Fatal("Cmd type is required")
-	}
+	// cmdType := flag.String("cmd", "", "クライアントコマンドの種類 (必須)")
+	// flag.Parse()
+	// if *cmdType == "" {
+	// 	zap.L().Fatal("Cmd type is required")
+	// }
+	// switch *cmdType {
+	// case "UserFollowers":
+	// 	cmd := cmd.UserFollowersCmdFromFlag()
+	// 	cmd.Execute()
+	// case "UserID":
+	// 	cmd := cmd.UserIDCmdFromFlag()
+	// 	cmd.Execute()
+	// case "UserFollowings":
+	// 	cmd := cmd.UserFollowingsCmdFromFlag()
+	// 	cmd.Execute()
+	// default:
+	// 	zap.L().Fatal("Cmd type is required")
+	// }
+	cmd := example.NewGroupUsersCmd()
+	cmd.Execute()
 }
