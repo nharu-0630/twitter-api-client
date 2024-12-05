1a97bfdc99fae53ee3bbabb8119fd05d693feebe
nharu-0630 <8305330+nharu-0630@users.noreply.github.com>
2024-12-05 15:16:26 +0900

    [add] フォロワーのクローラを追加

---
+++ cmd/brute_followers.go
@@ -0,0 +1,118 @@
+package cmd
+
+import (
+	"github.com/nharu-0630/twitter-api-client/api"
+	"github.com/nharu-0630/twitter-api-client/model"
+	"github.com/nharu-0630/twitter-api-client/util"
+	"go.uber.org/zap"
+)
+
+type BruteFollowersProps struct {
+	model.Config `yaml:",inline"`
+	MaxLimit     struct {
+		PerUser  int `yaml:"per_user"`
+		PerTweet int `yaml:"per_tweet"`
+	} `yaml:"max_limit"`
+	Queries []struct {
+		Key         string   `yaml:"key"`
+		ScreenNames []string `yaml:"screen_names"`
+	}
+}
+
+type BruteFollowers struct {
+	Props       BruteFollowersProps
+	ClientsPipe *api.ClientsPipe
+}
+
+func (bf *BruteFollowers) Execute() {
+	zap.L().Info("フォロワーのツイート取得を開始します", zap.String("config", bf.Props.Config.Name))
+	bf.ClientsPipe = api.NewClientsPipe(bf.Props.Config)
+
+	queryMap := make(map[string][]string)
+	queryMapIdx := make(map[string]int)
+	for _, query := range bf.Props.Queries {
+		queryMap[query.Key] = append(queryMap[query.Key], query.ScreenNames...)
+		queryMapIdx[query.Key] = 0
+	}
+
+	queryMapFollowers := make(map[string][]model.User)
+	queryMapFollowersIdx := make(map[string]int)
+	for {
+		flag := true
+		for key := range queryMap {
+			if queryMapIdx[key] >= len(queryMap[key]) {
+				continue
+			}
+			flag = false
+			screenName := queryMap[key][queryMapIdx[key]]
+			user, err := bf.ClientsPipe.UserByScreenName(screenName)
+			if err != nil {
+				zap.L().Error("ユーザー情報の取得に失敗しました", zap.String("screen_name", screenName), zap.Error(err))
+				continue
+			}
+			util.Log(bf.Props.Config.Name, []string{"User", screenName}, map[string]interface{}{"User": user}, false)
+
+			followers := []model.User{}
+			bottomCursor := ""
+			for {
+				res, cursor, err := bf.ClientsPipe.Followers(user.ID, bottomCursor)
+				if err != nil {
+					zap.L().Error("フォロワーの取得に失敗しました", zap.String("screen_name", screenName), zap.Error(err))
+					break
+				}
+				followers = append(followers, res...)
+				if cursor.IsAfterLast {
+					break
+				}
+				if len(followers) >= bf.Props.MaxLimit.PerUser {
+					break
+				}
+			}
+			util.Log(bf.Props.Config.Name, []string{"Followers", key, screenName}, map[string]interface{}{"Followers": followers}, false)
+
+			queryMapIdx[key]++
+
+			queryMapFollowers[key] = append(queryMapFollowers[key], followers...)
+			queryMapFollowersIdx[key] = 0
+		}
+		if flag {
+			break
+		}
+	}
+
+	for {
+		flag := true
+		for key := range queryMap {
+			if queryMapFollowersIdx[key] >= len(queryMapFollowers[key]) {
+				continue
+			}
+			flag = false
+			user := queryMapFollowers[key][queryMapFollowersIdx[key]]
+
+			tweets := []model.Tweet{}
+			bottomCursor := ""
+			for {
+				res, cursor, err := bf.ClientsPipe.SearchTimeline("(from:"+user.Legacy.ScreenName+")", bottomCursor)
+				if err != nil {
+					zap.L().Error("ツイートの取得に失敗しました", zap.String("screen_name", user.Legacy.ScreenName), zap.Error(err))
+					break
+				}
+				tweets = append(tweets, res...)
+				if cursor.IsAfterLast {
+					break
+				}
+				if len(tweets) >= bf.Props.MaxLimit.PerTweet {
+					break
+				}
+			}
+			util.Log(bf.Props.Config.Name, []string{"Tweets", key, user.Legacy.ScreenName}, map[string]interface{}{"Tweets": tweets}, false)
+
+			queryMapFollowersIdx[key]++
+		}
+		if flag {
+			break
+		}
+	}
+
+	zap.L().Info("フォロワーのツイート取得が完了しました", zap.String("config", bf.Props.Config.Name))
+}
